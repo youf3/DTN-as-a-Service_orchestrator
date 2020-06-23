2d430ad5bfbe9ff40404ac87195c75ec5dcb61e6
Se-young Yu <young.yu@northwestern.edu>
2020-06-23 15:13:46 -0500

    fixed error when transfer.end_time is none

---
+++ orchestrator/test.py
@@ -0,0 +1,88 @@
+from flask import Flask
+from flask_testing import TestCase
+import os
+import app
+import unittest
+
+class OrchestratorTest(TestCase):
+
+    def create_app(self):
+        app.app.config['TESTING'] = True
+        app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+        app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        return app.app
+
+    def setUp(self):
+        if os.path.exists('orchestrator/test.db'):
+            os.remove('orchestrator/test.db')
+        from app import db
+        db.create_all()
+        app.init_db()
+
+    def tearDown(self):
+        os.remove('orchestrator/test.db')
+
+    def test_running(self):
+        response = self.client.get('/')
+        result = response.data        
+        assert result == b'The orchestrator is running'
+
+    def test_add_DTN(self):
+        data = {
+            'name' : 'testDTN1',
+            'man_addr' : '192.168.1.1',
+            'data_addr' : '192.168.2.1',
+            'username' : 'nobody',
+            'interface' : 'eth0'
+        }
+        response = self.client.post('/DTN/',json=data)
+        result = response.get_json()
+        assert result == {'id' : 1}
+
+    def test_get_DTN(self):
+        self.test_add_DTN()
+        response = self.client.get('/DTN/1')
+        result = response.get_json()
+        assert result == {
+            'name' : 'testDTN1',
+            'man_addr' : '192.168.1.1',
+            'data_addr' : '192.168.2.1',
+            'id' : 1,
+            'username' : 'nobody',
+            'interface' : 'eth0'
+        }
+
+    def test_delete_DTN(self):
+        self.test_add_DTN()
+        response = self.client.delete('/DTN/1')
+        result = response.get_json()
+        assert result == {'id' : 1}
+
+        response = self.client.get('/DTN/1')
+        assert response.status_code == 404
+
+    def test_get_worker_types(self):
+        response = self.client.get('/worker_type')
+        result = response.get_json()
+        assert result == {
+            '1': 'OS_CONTROLLED',
+            '2': 'BIND_TO_NUMA',
+            '3': 'BIND_TO_CORE'
+        }
+
+    # def test_add_twice(self):
+    #     self.test_add_DTN()
+    #     data = {
+    #         'name' : 'testDTN1',
+    #         'man_addr' : '192.168.1.1',
+    #         'data_addr' : '192.168.2.1',
+    #         'username' : 'nobody'
+    #     }
+        
+    #     response = self.client.post('/DTN/', json=data)
+    #     result = response.get_json()
+    #     assert response.status_code == 400
+    #     assert result == {'message' : 'Unable to add DTN'}    
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
