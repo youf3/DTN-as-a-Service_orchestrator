f5342676deedb2f749632a554905d3e6db82cb70
Danial Ebling <danial@uen.org>
2021-10-19 15:22:09 -0600

    Changed orchestrator client to a python package, updated notebook container to jupyterhub for sso

---
+++ notebook/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dtn_orchestrator',
+    version='0.1.0',
+    packages=find_packages(include=['dtn_orchestrator', 'dtn_orchestrator.*'])
+)
